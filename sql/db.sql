04f73b15bdb81eb446abeb5d53aa67877458e0a8
Achmad Musyaffa Taufiqi <amusfq@gmail.com>
2021-07-05 07:40:27 +0700

    update db

--- sql/db.sql
+++ sql/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 28, 2021 at 05:21 AM
+-- Generation Time: Jul 05, 2021 at 12:39 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.3.24
 
@@ -24,12 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `berita`
+--
+
+CREATE TABLE `berita` (
+  `id` int(11) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `berita` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `berita`
+--
+
+INSERT INTO `berita` (`id`, `judul`, `berita`, `created_at`, `updated_at`) VALUES
+(1, 'Pedoman Akademik Universitas Narotama 2021', 'Dalam rangkan mendukung berlangsunya Tri Dharma perguruan tinggi, Universitas Narotama menerbitkan Pedoman Akademik yang dapat di unduh pada', '2021-07-04 23:22:16', '2021-07-04 23:27:44'),
+(2, 'Peraturan rektor tentang Tugas Akhir', 'Berikut adalah Peraturan Rektor mengenai Pelaksanaan Tugas Akhir, Skripsi dan Tesis di lingkungan Universitas Narotama Surabaya Tahun 2018', '2021-07-04 23:27:09', '2021-07-04 23:27:09');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `biodata`
 --
 
 CREATE TABLE `biodata` (
-  `nim` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `nama_lengkap` varchar(255) NOT NULL,
+  `foto` varchar(255) NOT NULL DEFAULT 'assets/img/default.jpg',
   `jenis_kelamin` tinyint(4) NOT NULL DEFAULT '0',
   `tempat_lahir` varchar(64) NOT NULL,
   `tanggal_lahir` date NOT NULL,
@@ -37,11 +61,7 @@ CREATE TABLE `biodata` (
   `agama` tinyint(4) NOT NULL DEFAULT '0',
   `alamat` text NOT NULL,
   `no_telepon` varchar(13) NOT NULL,
-  `id_dosen` int(11) NOT NULL,
-  `id_prodi` int(11) NOT NULL,
-  `semester` int(11) NOT NULL DEFAULT '1',
   `status` int(11) NOT NULL DEFAULT '1' COMMENT '0 = tidak aktif\r\n1 = aktif',
-  `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -50,9 +70,9 @@ CREATE TABLE `biodata` (
 -- Dumping data for table `biodata`
 --
 
-INSERT INTO `biodata` (`nim`, `nama_lengkap`, `jenis_kelamin`, `tempat_lahir`, `tanggal_lahir`, `golongan_darah`, `agama`, `alamat`, `no_telepon`, `id_dosen`, `id_prodi`, `semester`, `status`, `password`, `created_at`, `updated_at`) VALUES
-(4319023, 'Achmad Musyaffa Taufiqi', 0, 'Tangerang', '2000-01-25', 0, 0, 'Ds Terung Kulon, Krian, Sidoarjo, Jawa Timur', '089612890670', 1, 1, 1, 1, '21232f297a57a5a743894a0e4a801fc3', '2021-06-21 09:33:56', '2021-06-28 04:18:50'),
-(4319028, 'Larojas Putra Buton', 0, 'Surabaya', '1999-01-01', 0, 1, 'ini alamat', '123123', 2, 1, 1, 1, '1a29026d9efdf17b7f3792dcc72a765e', '2021-06-28 03:45:01', '2021-06-28 03:45:01');
+INSERT INTO `biodata` (`id`, `id_user`, `nama_lengkap`, `foto`, `jenis_kelamin`, `tempat_lahir`, `tanggal_lahir`, `golongan_darah`, `agama`, `alamat`, `no_telepon`, `status`, `created_at`, `updated_at`) VALUES
+(4319029, 3, 'Super Admin', 'uploads/1625438851.jpg', 0, 'Tangerang', '2000-01-25', 0, 0, 'Sidoarjo', '04389', 1, '2021-06-30 15:06:04', '2021-07-04 22:47:31'),
+(4319049, 38, 'Achmad Musyaffa Taufiqi', 'assets/img/default.jpg', 0, 'Tangerang', '2000-01-25', 0, 0, 'Sidoarjo', '089121313', 1, '2021-07-04 23:32:00', '2021-07-04 23:32:00');
 
 -- --------------------------------------------------------
 
@@ -116,7 +136,9 @@ CREATE TABLE `jadwal_kuliah` (
 
 INSERT INTO `jadwal_kuliah` (`id`, `id_mata_kuliah`, `id_dosen`, `jam_kuliah`, `hari_kuliah`, `ruang`, `created_at`, `updated_at`) VALUES
 (1, 2, 1, '08:30:00', 'Senin', 'A-10', '2021-06-22 03:45:27', '2021-06-22 09:24:27'),
-(2, 1, 2, '10:30:00', 'Senin', 'B-01', '2021-06-22 03:45:27', '2021-06-22 09:24:30');
+(2, 1, 2, '10:30:00', 'Senin', 'B-01', '2021-06-22 03:45:27', '2021-06-22 09:24:30'),
+(3, 6, 1, '08:30:00', 'Selasa', 'A-10', '2021-07-05 00:09:26', '2021-07-05 00:09:26'),
+(4, 3, 2, '10:30:00', 'Selasa', 'A-01', '2021-07-05 00:20:16', '2021-07-05 00:22:50');
 
 -- --------------------------------------------------------
 
@@ -127,7 +149,7 @@ INSERT INTO `jadwal_kuliah` (`id`, `id_mata_kuliah`, `id_dosen`, `jam_kuliah`, `
 CREATE TABLE `jadwal_mahasiswa` (
   `id` int(11) NOT NULL,
   `id_jadwal_kuliah` int(11) NOT NULL,
-  `id_mahasiswa` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `semester` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -137,8 +159,33 @@ CREATE TABLE `jadwal_mahasiswa` (
 -- Dumping data for table `jadwal_mahasiswa`
 --
 
-INSERT INTO `jadwal_mahasiswa` (`id`, `id_jadwal_kuliah`, `id_mahasiswa`, `semester`, `created_at`, `updated_at`) VALUES
-(15, 2, 4319023, 1, '2021-06-28 05:06:39', '2021-06-28 05:06:39');
+INSERT INTO `jadwal_mahasiswa` (`id`, `id_jadwal_kuliah`, `id_user`, `semester`, `created_at`, `updated_at`) VALUES
+(20, 2, 38, 1, '2021-07-05 00:09:42', '2021-07-05 00:09:42'),
+(21, 1, 38, 1, '2021-07-05 00:09:44', '2021-07-05 00:09:44'),
+(22, 3, 38, 1, '2021-07-05 00:09:46', '2021-07-05 00:09:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_dosen` int(11) NOT NULL,
+  `id_prodi` int(11) NOT NULL,
+  `semester` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `id_user`, `id_dosen`, `id_prodi`, `semester`, `created_at`, `updated_at`) VALUES
+(5, 38, 1, 1, 1, '2021-07-04 23:32:00', '2021-07-04 23:32:00');
 
 -- --------------------------------------------------------
 
@@ -161,8 +208,10 @@ CREATE TABLE `mata_kuliah` (
 --
 
 INSERT INTO `mata_kuliah` (`id`, `nama_mata_kuliah`, `id_prodi`, `sks`, `semester`, `created_at`, `updated_at`) VALUES
-(1, 'Basis Data', 1, 2, 1, '2021-06-22 03:44:09', '2021-06-22 03:44:09'),
-(2, 'Algoritma Pemrograman', 1, 2, 1, '2021-06-22 03:44:09', '2021-06-22 03:44:09');
+(1, 'Basis Data', 2, 2, 1, '2021-06-22 03:44:09', '2021-07-04 22:28:21'),
+(2, 'Algoritma Pemrograman', 1, 2, 1, '2021-06-22 03:44:09', '2021-06-22 03:44:09'),
+(3, 'Desain dan Pemrograman Web', 1, 2, 1, '2021-07-04 11:47:59', '2021-07-04 11:47:59'),
+(6, 'Desain Grafis', 2, 3, 1, '2021-07-04 23:31:25', '2021-07-04 23:31:25');
 
 -- --------------------------------------------------------
 
@@ -183,19 +232,70 @@ CREATE TABLE `prodi` (
 --
 
 INSERT INTO `prodi` (`id`, `nama_prodi`, `id_fakultas`, `created_at`, `updated_at`) VALUES
-(1, 'Teknik Informatika', 1, '2021-06-21 09:32:52', '2021-06-21 09:32:52');
+(1, 'Teknik Informatika', 1, '2021-06-21 09:32:52', '2021-06-21 09:32:52'),
+(2, 'Sistem Informasi', 1, '2021-07-04 22:28:01', '2021-07-04 22:28:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `nama_role` varchar(25) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `nama_role`, `created_at`, `updated_at`) VALUES
+(1, 'admin', '2021-06-30 14:28:44', '2021-06-30 14:28:44'),
+(2, 'mahasiswa', '2021-06-30 14:28:44', '2021-06-30 14:28:44'),
+(3, 'dosen', '2021-06-30 14:28:50', '2021-06-30 14:28:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `id_role`, `username`, `password`, `created_at`, `updated_at`) VALUES
+(3, 1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2021-06-30 15:05:28', '2021-07-03 23:37:03'),
+(38, 2, '04319023', '0344a49d08215c613d862c751323402a', '2021-07-04 23:32:00', '2021-07-04 23:32:00');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `berita`
+--
+ALTER TABLE `berita`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `biodata`
 --
 ALTER TABLE `biodata`
-  ADD PRIMARY KEY (`nim`),
-  ADD KEY `id_prodi` (`id_prodi`),
-  ADD KEY `id_dosen` (`id_dosen`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `dosen`
@@ -223,7 +323,16 @@ ALTER TABLE `jadwal_kuliah`
 ALTER TABLE `jadwal_mahasiswa`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_jadwal_kuliah` (`id_jadwal_kuliah`),
-  ADD KEY `nim` (`id_mahasiswa`);
+  ADD KEY `nim` (`id_user`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`,`id_dosen`,`id_prodi`),
+  ADD KEY `id_dosen` (`id_dosen`),
+  ADD KEY `id_prodi` (`id_prodi`);
 
 --
 -- Indexes for table `mata_kuliah`
@@ -240,14 +349,33 @@ ALTER TABLE `prodi`
   ADD KEY `id_fakultas` (`id_fakultas`);
 
 --
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_role` (`id_role`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `berita`
+--
+ALTER TABLE `berita`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `biodata`
 --
 ALTER TABLE `biodata`
-  MODIFY `nim` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4319029;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4319050;
 
 --
 -- AUTO_INCREMENT for table `dosen`
@@ -265,25 +393,43 @@ ALTER TABLE `fakultas`
 -- AUTO_INCREMENT for table `jadwal_kuliah`
 --
 ALTER TABLE `jadwal_kuliah`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `jadwal_mahasiswa`
 --
 ALTER TABLE `jadwal_mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `mata_kuliah`
 --
 ALTER TABLE `mata_kuliah`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `prodi`
 --
 ALTER TABLE `prodi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- Constraints for dumped tables
@@ -293,8 +439,7 @@ ALTER TABLE `prodi`
 -- Constraints for table `biodata`
 --
 ALTER TABLE `biodata`
-  ADD CONSTRAINT `biodata_ibfk_1` FOREIGN KEY (`id_prodi`) REFERENCES `prodi` (`id`),
-  ADD CONSTRAINT `biodata_ibfk_2` FOREIGN KEY (`id_dosen`) REFERENCES `dosen` (`nip`);
+  ADD CONSTRAINT `biodata_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `jadwal_kuliah`
@@ -308,7 +453,15 @@ ALTER TABLE `jadwal_kuliah`
 --
 ALTER TABLE `jadwal_mahasiswa`
   ADD CONSTRAINT `jadwal_mahasiswa_ibfk_1` FOREIGN KEY (`id_jadwal_kuliah`) REFERENCES `jadwal_kuliah` (`id`),
-  ADD CONSTRAINT `jadwal_mahasiswa_ibfk_2` FOREIGN KEY (`id_mahasiswa`) REFERENCES `biodata` (`nim`);
+  ADD CONSTRAINT `jadwal_mahasiswa_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `mahasiswa_ibfk_2` FOREIGN KEY (`id_dosen`) REFERENCES `dosen` (`nip`),
+  ADD CONSTRAINT `mahasiswa_ibfk_3` FOREIGN KEY (`id_prodi`) REFERENCES `prodi` (`id`);
 
 --
 -- Constraints for table `mata_kuliah`
@@ -321,6 +474,12 @@ ALTER TABLE `mata_kuliah`
 --
 ALTER TABLE `prodi`
   ADD CONSTRAINT `prodi_ibfk_1` FOREIGN KEY (`id_fakultas`) REFERENCES `fakultas` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
